9166d11d85aad294071e360970a7df55dcbe8dea
Mateusz <187835@edu.p.lodz.pl>
2016-05-20 22:21:07 +0200

    Dodanie klasy prawdopodobnie potrzebnej do przekazania listy obecności z jsp do kontrolera

---
+++ src/main/java/neo/dmcs/view/precense/CheckPrecenseViewWrapper.java
@@ -0,0 +1,19 @@
+package neo.dmcs.view.precense;
+
+import java.util.List;
+
+/**
+ * @Author Mateusz Wieczorek, 19.05.16.
+ */
+public class CheckPrecenseViewWrapper {
+
+    private List<CheckPrecenseView> students;
+
+    public List<CheckPrecenseView> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<CheckPrecenseView> students) {
+        this.students = students;
+    }
+}
