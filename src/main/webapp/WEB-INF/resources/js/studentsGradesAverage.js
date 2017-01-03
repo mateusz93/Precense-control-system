8398281e63cb634edf948980f3ad959e424fa579
Mateusz Wieczorek <wskmateusz@gmail.com>
2017-01-03 18:57:24 +0100

    more example stats

--- src/main/webapp/WEB-INF/resources/js/studentsGradesAverage.js
+++ src/main/webapp/WEB-INF/resources/js/studentsGradesAverage.js
@@ -19,6 +19,7 @@ function drawChart() {
     var chart = new google.visualization.PieChart(document.getElementById('globalGradesAverage'));
     chart.draw(data, {width: 400, height: 300});
 
+
     var jsonData = $.ajax({
         url: "http://localhost:8080/stats/finalGradesAverage",
         dataType: "json",
@@ -31,4 +32,50 @@ function drawChart() {
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.PieChart(document.getElementById('finalGradesAverage'));
     chart.draw(data, {width: 400, height: 300});
+
+
+/*    var jsonData = $.ajax({
+        url: "http://localhost:8080/stats/subjectGradesAverage",
+        dataType: "json",
+        async: false
+        }).responseText;
+
+    // Create our data table out of JSON data loaded from server.
+    var data = new google.visualization.arrayToDataTable(jsonData);
+    var options = {
+      width: 800,
+      height: 300
+      axes: {
+        x: {
+          0: { side: 'bottom', label: 'Nazwa przedmiotu'} // Top x-axis.
+        }
+      },
+      bar: { groupWidth: "90%" }
+    };
+
+    var chart = new google.charts.ColumnChart(document.getElementById('subjectGradesAverage'));
+    chart.draw(data, {width: 400, height: 300});*/
+
+
+       // Define the chart to be drawn.
+       var data = google.visualization.arrayToDataTable([
+            ['Nazwa', 'Srednia', {role: 'style'}],
+            ['Matematyka',  3.5, '#76A7FA'],
+            ['Język Angielski',  4.3, '#76A7FA'],
+            ['Historia',  3.88, '#76A7FA'],
+            ['Biologia',  4.55, '#76A7FA'],
+            ['Geografia',  3.9, '#76A7FA'],
+            ['Język Polski',  5.0, '#76A7FA'],
+            ['WF',  5.2, '#76A7FA']
+          ]);
+
+       var options = {
+          width: 800,
+          height: 300
+       };
+
+       // Instantiate and draw the chart.
+       var chart = new google.visualization.ColumnChart(document.getElementById('subjectGradesAverage'));
+       chart.draw(data, options);
+
 }
